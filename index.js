bb5523267dc5c1cdb9f6bb31dae737367cfee22d
ls1955 <xxlsxx1955@gmail.com>
2023-07-19 21:45:08 +0800

    Include TicTacToeGameController
    Because:
    There should be something that handle the main logic
    and loop of the game.

--- index.js
+++ index.js
@@ -31,4 +31,32 @@ function PlayerFactory(name, symbol) {
     }
 }
 
-module.exports = [GameBoard, PlayerFactory]
+function TicTacToeGameController(playerOne, playerTwo) {
+    const players = [playerOne, playerTwo]
+    const board = GameBoard()
+    let activePlayer = players[0]
+
+    const playRound = (row_index, col_index) => {
+        console.log(`It is ${activePlayer.name}'s turn.`)
+
+        board.markBoard(activePlayer.symbol, row_index, col_index)
+
+        // TODO: What if there is a winner, or there is a tie?
+
+        switchActivePlayer()
+    }
+
+    const getActivePlayer = () => activePlayer
+
+    const switchActivePlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0] 
+    }
+
+    return {
+        getActivePlayer,
+        playRound,
+        getBoard: board.getBoard
+    }
+}
+
+module.exports = [GameBoard, PlayerFactory, TicTacToeGameController]
